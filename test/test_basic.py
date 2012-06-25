e5b31f85b09e70e9ffb569df6d9dcee3060e2f60
Sean Reifschneider <jafo@tummy.com>
2012-06-25 14:33:44 -0600

    Added basic test infrastructure.

---
+++ test/test_basic.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#
+#  Run basic tests of makepath.
+
+import sys
+sys.path.append('.')
+sys.path.append('..')
+import os
+
+import unittest
+
+class Test(unittest.TestCase):
+	@classmethod
+	def setUp(self):
+		self.testdir = os.path.join('/tmp/makepath-test.%s' % os.getpid())
+		if os.path.exists(self.testdir):
+			os.system('rm -rf "%s"' % self.testdir)
+		os.chdir(self.testdir)
+	
+	@classmethod
+	def tearDown(self):
+		if os.path.exists(self.testdir):
+			os.system('rm -rf "%s"' % self.testdir)
