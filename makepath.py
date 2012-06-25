dd92be55264115af90e32ec91cbb8f1b96056f21
Sean Reifschneider <jafo@tummy.com>
2012-06-25 16:43:10 -0600

    Changing so that multiple directories can be passed to makepath.

--- makepath.py
+++ makepath.py
@@ -5,8 +5,9 @@
 import os
 
 
-def makepath(directory_name):
-	if not directory_name or os.path.exists(directory_name):
-		return
-	makepath(os.path.dirname(directory_name))
-	os.mkdir(directory_name)
+def makepath(*args):
+	for directory_name in args:
+		if not directory_name or os.path.exists(directory_name):
+			continue
+		makepath(os.path.dirname(directory_name))
+		os.mkdir(directory_name)
